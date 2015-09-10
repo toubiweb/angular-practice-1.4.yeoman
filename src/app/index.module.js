c5abc07c4826f9673b927b30f08f6be80c7a46d2
Toub <n.toublanc@gmail.com>
2015-09-10 09:16:24 +0100

    fix

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,15 +4,16 @@
     // laxcomma define application module and dependencies
     angular.module('tpAngular', [
         'ngAnimate' // CSS-based animations: https://code.angularjs.org/1.4.5/docs/api/ngAnimate
-        ,'ngCookies' // cookies management: https://code.angularjs.org/1.4.5/docs/api/ngCookies
-        ,'ngTouch' // touch events for tablets & mobiles: https://code.angularjs.org/1.4.5/docs/api/ngTouch
-        ,'ngMessages' // form error messages: https://code.angularjs.org/1.4.5/docs/api/ngMessages
-        ,'ngSanitize' // safe HTML: https://code.angularjs.org/1.4.5/docs/api/ngSanitize
-        ,'ui.router' // Angular-UI router: https://github.com/angular-ui/ui-router
-        ,'mgcrea.ngStrap' // AngularJS directives for Bootstrap:  https://github.com/mgcrea/angular-strap
-        ,'toastr' // angular notifications: https://github.com/Foxandxss/angular-toastr
-        ,'tpAngular.profile' // profile
-        ,'tpAngular.form' // form
+        , 'ngResource' // RESTful services: https://docs.angularjs.org/1.4.5/docs/api/ngResource
+        , 'ngCookies' // cookies management: https://code.angularjs.org/1.4.5/docs/api/ngCookies
+        , 'ngTouch' // touch events for tablets & mobiles: https://code.angularjs.org/1.4.5/docs/api/ngTouch
+        , 'ngMessages' // form error messages: https://code.angularjs.org/1.4.5/docs/api/ngMessages
+        , 'ngSanitize' // safe HTML: https://code.angularjs.org/1.4.5/docs/api/ngSanitize
+        , 'ui.router' // Angular-UI router: https://github.com/angular-ui/ui-router
+        , 'mgcrea.ngStrap' // AngularJS directives for Bootstrap:  https://github.com/mgcrea/angular-strap
+        , 'toastr' // angular notifications: https://github.com/Foxandxss/angular-toastr
+        , 'tpAngular.profile' // profile
+        , 'tpAngular.form' // form
     ]);
 
 })();
